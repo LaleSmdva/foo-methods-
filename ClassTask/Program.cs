342161b57b5c678ffb3fb9c8141f39006b6ead9c
LaleSmdva <6vrseyy@code.edu.az>
2022-11-17 11:33:28 +0400

    first commit

---
+++ ClassTask/Program.cs
@@ -0,0 +1,104 @@
+﻿#region minTapma
+// metod intden array qebul edir, min tapir
+
+//int Min(int[] arr)
+//{
+//    int min = arr[0];
+//    for (int i = 0; i < arr.Length; i++)
+//	{
+//		if (arr[i]<min)
+//		{
+//			min = arr[i];
+//		}
+//	}
+//	return min;
+//}
+//Console.WriteLine(Min(new int[] { 1, 2, 3, 1 })	);
+#endregion
+#region indeksDeyerToplama
+//int Sum(int[]arr)
+//{
+//	int sum = 0;
+//	foreach (var item in arr)
+//	{
+//		sum += item;
+//	}
+//	return sum;
+//}
+//Console.WriteLine(Sum(new int[] { 1, 2, 3 }) ); 
+#endregion
+#region reverse
+//using System.Xml.XPath;
+//int[] arr = { 1, 2, 3, 4, 5 };
+
+//int result = 0;
+//int Reverse(int[] arrx)
+//{
+//    int[] reverseArr = new int[arr.Length];
+//    for (int i = arr.Length - 1; i >= 0; i--)
+//	{
+//		result = reverseArr[i];
+//	}
+//	return result;
+//}
+//Console.WriteLine(Reverse(new int[] { 1, 2, 3, 4, 5 }));
+#endregion
+#region value hansi indeksdedir
+//array ve deyer qebul edir, necenci indeksde oldugunu gosterir arrde olmuyanda -1
+
+//int IndexOf(int[]arr,int value)
+//{
+//    for (int i = 0; i < arr.Length; i++)
+//    {
+//        if (arr[i] == value)
+//        {
+//            return i;
+//        }
+//    }
+//    return -1;
+//}
+//Console.WriteLine(IndexOf((new int[] { 4, 8, 0, 9 }), -5) ); 
+
+#endregion
+
+#region indeksinDeyeri
+
+#endregion
+
+
+#region replace letter
+//string word = "test";
+
+//string Replace(string word,char oldLetter,char newLetter)
+//{
+//    string newWord = "";
+//    for (int i = 0; i < word.Length; i++)
+//	{
+//		if(word[i]==oldLetter )
+//		{
+//			newWord += newLetter;
+//		}
+//		else
+//		{
+//			newWord += word[i];
+//		}
+//	}
+//	return newWord;
+//}
+//Console.WriteLine(Replace(word,'t','d'));
+#endregion
+
+#region reverseText
+//string text = "salam";
+//string reverseText = "";
+//string Reverse(string text)
+//{
+//	for (int i = text.Length - 1; i >= 0; i--)
+//	{
+//		reverseText += text[i];
+//	}
+//	return reverseText;
+//}
+//Console.WriteLine("Reverse is: " + Reverse("lala"));
+#endregion
+
